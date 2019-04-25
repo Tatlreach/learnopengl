12406fd1a0a142cdf1a9adee439c9b431ffddd8a
Mike-RyzenPC\Mike <Tatlreach@gmail.com>
2019-04-25 12:56:54 -0500

    added basic DrawArrays call.
    made CreateShader to create empty program. create/compile both shaders with CompileShader. Attache the shaders. Link the created program. Validate the program.
    
    CompileShader (used by CreateShader) creates an empty shader, writes passed into src to it, and compiles the shader. retuns shader id.

--- learnOpenGL1/src/Application.cpp
+++ learnOpenGL1/src/Application.cpp
@@ -1,7 +1,35 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-//#include <iostream>
+#include <iostream>
+
+static unsigned int CompileShader(unsigned int type, const std::string source ) {
+	unsigned int id = glCreateShader(GL_VERTEX_SHADER);
+	const char* src = source.c_str();	//return pointer to null terminated mutable array
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	//TODO: Error Handling
+
+	return id;
+}
+
+//returns an ID for the shader
+static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	//each of these are program IDs
+	//can use GLuint  for type too
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader );
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	//link 2 shaders into 1 file
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+}
 
 int main(void)
 {
@@ -40,8 +68,8 @@ int main(void)
 
 
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0,	
-		2,	//number of components in ea. vertex
+	glVertexAttribPointer(0,	//starting index of specified attribute
+		2,						//number of components in ea. vertex
 		GL_FLOAT,			//Attribute Type
 		GL_FALSE,			//normalized
 		sizeof(float)*2,	//stride, total size of vertex
@@ -56,6 +84,8 @@ int main(void)
 
 		//RENDER
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		//swap front & back buffers
 		glfwSwapBuffers(window);
