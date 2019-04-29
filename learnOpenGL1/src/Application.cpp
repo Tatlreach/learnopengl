2780949b6661faab095638ae450486ac252f4274
Mike-RyzenPC\Mike <Tatlreach@gmail.com>
2019-04-29 12:54:51 -0500

    fixed bug to compile shader of passed in type;   shader error handle/print out;   Deleted shaders after compile;   wrote shader text;   assigned shader text;   Deleted the shaderprogram at end;

--- learnOpenGL1/src/Application.cpp
+++ learnOpenGL1/src/Application.cpp
@@ -4,18 +4,32 @@
 #include <iostream>
 
 static unsigned int CompileShader(unsigned int type, const std::string source ) {
-	unsigned int id = glCreateShader(GL_VERTEX_SHADER);	//creates empty shader program to hold string src code
+	unsigned int id = glCreateShader(type);	//creates empty shader program to hold string src code
 	const char* src = source.c_str();	//return pointer to null terminated mutable array
 	glShaderSource(id, 1, &src, nullptr);	//Replaces source code in shader object
-	glCompileShader(id);
+	glCompileShader(id);				//compiles the string code in the shader
 
-	//TODO: Error Handling
+	//Error Handling
+	//query the compile to see errors
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);	//i: integer, v: vector
+	if (result == GL_FALSE) {
+		//did not compile
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*) alloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "failed to compile " << (type==GL_VERTEX_SHADER? "vertex" : "fragment") <<" shader" << '\n';
+		std::cout << message << std::endl;
+		glDeleteShader(id);
+		return 0;
+	}
 
 	return id;
 }
 
 //returns an ID for the shader
-static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
 {
 	//each of these are program IDs
 	//can use GLuint  for type too
@@ -27,13 +41,19 @@ static int CreateShader(const std::string& vertexShader, const std::string& frag
 	glAttachShader(program, vs);
 	glAttachShader(program, fs);
 	glLinkProgram(program);
-	glValidateProgram(program);
+	glValidateProgram(program);		//checks if program is runnable given state, if succeeds GL_TRUE is stored as part of the program object's state
 
+	//glDetachShader right away removes a lot of debugging options
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
 }
 
 int main(void)
 {
-	
+
 	GLFWwindow* window;
 
 	//init library
@@ -72,12 +92,33 @@ int main(void)
 		2,						//number of components in ea. vertex
 		GL_FLOAT,			//Attribute Type
 		GL_FALSE,			//normalized
-		sizeof(float)*2,	//stride, total size of vertex
+		sizeof(float) * 2,	//stride, total size of vertex
 		0);					//offset for the attribute WITHIN the vertex e.g. (const void*)8
 
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);	//bind to empty buffer
 
+	std::string vertexShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) in vec4 position;\n"	//accesses vertex buffer at index 0, automatically converted to vec4
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = position;\n"		//gl_Position is a vec4
+		"}\n";
+
+	std::string fragmentShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) out vec4 color;\n"	//accesses vertex buffer at index 0, automatically converted to vec4
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"		
+		"}\n";
+	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	glUseProgram(shader);
 
 	//loop until window closed
 	while (!glfwWindowShouldClose(window)) {
@@ -94,6 +135,7 @@ int main(void)
 		glfwPollEvents();
 	}
 
+	glDeleteProgram(shader);
 	glfwTerminate();
 	return 0;
 }
